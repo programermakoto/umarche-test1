5eae8da4f0f97bbd2c874c298f18d48800082da9
Terauchi Makoto <makoto@Terauchis-MacBook-Air.local>
2024-06-03 15:50:40 +0900

    component2

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,1 +1,8 @@
-component-test2
+<x-tests.app>
+    <x-slot name="header">
+        header2
+
+    </x-slot>
+    component-test2
+    <x-tests.card title="カードのタイトル2" content="カードのコンテント2" />
+</x-tests.app>
