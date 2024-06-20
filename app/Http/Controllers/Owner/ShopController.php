cb49167c264702411455f20014de614d412592c0
Terauchi Makoto <makoto@Terauchis-MacBook-Air.local>
2024-06-20 10:39:51 +0900

    formcheck

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-
+use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 class ShopController extends Controller
 {
     public function __construct()
@@ -55,16 +56,26 @@ class ShopController extends Controller
         //http://127.0.0.1:8000/owner/shops/edit/1
         return view("owner.shops.edit", compact("shop"));
     }
-    public function update(Request $request)
+    public function update(UploadImageRequest $request)
     {
-        $imageFile=$request->image;//imgを受け取り変数へ
+        $imageFile = $request->image;//imgを受け取り変数へ
 
-        if(!is_null($imageFile) && $imageFile->isValid()){//nullではないかアップロードできてるか確認
-        
-        Storage::putFile("public/shops",$imageFile);//保存先と保存したいファイル
-        
+        if (!is_null($imageFile) && $imageFile->isValid()) {//nullではないかアップロードできてるか確認
+
+            // Storage::putFile("public/shops",$imageFile);//保存先と保存したいファイル
+
+            $fileName = uniqid(rand() . "_");//ランダムなファイルを作成
+
+            $extension = $imageFile->extension();//extensionで受け取った画像の拡張子をつけて代入
+
+            $fileNameToStore = $fileName . "." . $extension;
+
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();//resizeでサイズ設定,encodeで画像として扱える
+
+            Storage::put("public/shops/" . $fileNameToStore, $resizedImage);//ファイルからのファイル名,リサイズした画像
+
         }
-        
+
         return redirect()->route("owner.shops.index");
     }
 }
