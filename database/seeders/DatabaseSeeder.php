1950cd208f3e0bce9aea54d016cc25f946cb52d5
Terauchi Makoto <makoto@Terauchis-MacBook-Air.local>
2024-07-01 09:47:37 +0900

    Product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use App\Models\Product;
+use App\Models\Stock;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -31,6 +32,9 @@ class DatabaseSeeder extends Seeder
             // ProductSeeder::class,
 
             // StockSeeder::class,
-            ]);
+        ]);
+        Product::factory(100)->create();
+
+        Stock::factory(100)->create();
     }
 }
